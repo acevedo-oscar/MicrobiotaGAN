6490c2e18ea59ed5332158f1771bc37c2cdcfec4
acevedo-oscar <oscar.acevedo@comunidad.unam.mx>
2018-11-28 21:50:24 -0600

    fixed indentations and so on

--- dataset_manager.py
+++ dataset_manager.py
@@ -1,58 +1,59 @@
 import numpy
 
+
 from MicrobiotaGAN.utilities import normalize_ds
 
-class dataset_manager:
 
-	def __init__(self, images_set, norm=False) -> None:
+class DataSetManager:
 
-		if norm == False:
-			ds = images_set
-		else:
-			ds = normalize_ds(images_set)
+    def __init__(self, images_set, norm=False) -> None:
 
-		self._images = ds
-		self._num_examples = images_set.shape[0]
-		self._epochs_completed : int = 0
-		self._index_in_epoch = 0
+        if norm:
+            ds = normalize_ds(images_set)
+        else:
+            ds = images_set
 
-
-	@property
-	def images(self):
-		return self._images
+        self._images = ds
+        self._num_examples = images_set.shape[0]
+        self._epochs_completed : int = 0
+        self._index_in_epoch = 0
 
 
-	def next_batch(self, batch_size, shuffle=True):
-		"""Return the next `batch_size` examples from this data set."""
+    @property
+    def images(self):
+        return self._images
 
-		start = self._index_in_epoch
-		# Shuffle for the first epoch
-		if self._epochs_completed == 0 and start == 0 and shuffle:
-			perm0 = numpy.arange(self._num_examples)
-			numpy.random.shuffle(perm0)
-			self._images = self.images[perm0]
+    def next_batch(self, batch_size, shuffle=True):
+        """Return the next `batch_size` examples from this data set."""
 
-		# Go to the next epoch
-		if start + batch_size > self._num_examples:
-			# Finished epoch
-			self._epochs_completed += 1
-			# Get the rest examples in this epoch
-			rest_num_examples = self._num_examples - start
-			images_rest_part = self._images[start:self._num_examples]
-			# Shuffle the data
-			if shuffle:
-				perm = numpy.arange(self._num_examples)
-				numpy.random.shuffle(perm)
-				self._images = self.images[perm]
+        start = self._index_in_epoch
+        # Shuffle for the first epoch
+        if self._epochs_completed == 0 and start == 0 and shuffle:
+            perm0 = numpy.arange(self._num_examples)
+            numpy.random.shuffle(perm0)
+            self._images = self.images[perm0]
 
-			# Start next epoch
-			start = 0
-			self._index_in_epoch = batch_size - rest_num_examples
-			end = self._index_in_epoch
-			images_new_part = self._images[start:end]
+        # Go to the next epoch
+        if start + batch_size > self._num_examples:
+            # Finished epoch
+            self._epochs_completed += 1
+            # Get the rest examples in this epoch
+            rest_num_examples = self._num_examples - start
+            images_rest_part = self._images[start:self._num_examples]
+             # Shuffle the data
+            if shuffle:
+                perm = numpy.arange(self._num_examples)
+                numpy.random.shuffle(perm)
+                self._images = self.images[perm]
 
-			return numpy.concatenate( (images_rest_part, images_new_part), axis=0)
-		else:
-			self._index_in_epoch += batch_size
-			end = self._index_in_epoch
-			return self._images[start:end]
+            # Start next epoch
+            start = 0
+            self._index_in_epoch = batch_size - rest_num_examples
+            end = self._index_in_epoch
+            images_new_part = self._images[start:end]
+
+            return numpy.concatenate( (images_rest_part, images_new_part), axis=0)
+        else:
+            self._index_in_epoch += batch_size
+            end = self._index_in_epoch
+            return self._images[start:end]
